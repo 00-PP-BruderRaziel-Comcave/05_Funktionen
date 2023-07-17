d55a792b1741e414478c5b295b0e5a4277b672a5
Nico <nico.aldag@web.de>
2023-07-17 11:01:45 +0200

    Parametrisierung & Datenübergabe

--- script_01.js
+++ script_01.js
@@ -4,14 +4,39 @@
 // 1. Kapselung von Codeblöcken
 
 // Funktionsaufruf (call)
-test();
+// test();
 
 // Funktionsrumpf (body) | callee
 // Funktionsdeklaration
-function test()
-{  
-    console.log("Hallo Tobias");
+// function test()
+// {  
+//     console.log("Hallo Tobias");
+// }
+
+/***** Funktionn 02a *******/
+// 2a. Parametisierung + Datenübergabe von Innen
+
+// ausgabeNamen();
+
+function ausgabeNamen() {
+    // What happens in VEGAS....
+    let firstName = "Nico";
+    console.log("Hallo " + firstName + "!");
 }
 
+// console.log(firstName);     //Fehler --> SCOPE!
 
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+// Argumente sind Daten für Parameter
+ausgabeNamen2("Sohrab");
+ausgabeNamen2("Nico");
+ausgabeNamen2("Yasmeen");
+ausgabeNamen2(prompt("Und dein Name ist...?"))
+
+// Funktion mit Parameter(n)
+function ausgabeNamen2(firstName) {     
+    console.log("Hallo " + firstName + "!");
+}
 
